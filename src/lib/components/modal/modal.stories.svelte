29cd9a2f68b162f08a2954ad3b149ef7e86250cf
Thomas Eckert <thomas.james.eckert@gmail.com>
2025-05-29 22:11:43 -0400

    Set fonts and dark variant

---
+++ src/lib/components/modal/modal.stories.svelte
@@ -0,0 +1,22 @@
+<script module>
+	import { defineMeta } from "@storybook/addon-svelte-csf"
+	import Modal from "./modal.svelte"
+	import { fn } from "storybook/test"
+
+	// More on how to set up stories at: https://storybook.js.org/docs/writing-stories
+	const { Story } = defineMeta({
+		title: "Components/Modal",
+		component: Modal,
+		tags: ["autodocs"],
+		argTypes: {
+			backgroundColor: { control: "color" },
+			size: {
+				control: { type: "select" },
+				options: ["small", "medium", "large"]
+			}
+		},
+		args: {
+			onclick: fn()
+		}
+	})
+</script>
