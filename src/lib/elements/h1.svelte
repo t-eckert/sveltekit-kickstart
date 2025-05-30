8cd4780b28469b6585e5a87e2c03bfd4998c1310
Thomas Eckert <thomas.james.eckert@gmail.com>
2025-05-29 22:14:56 -0400

    Add H1 element

---
+++ src/lib/elements/h1.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+	let { children } = $props()
+</script>
+
+<h1 class="text-3xl font-semibold tracking-tight text-gray-900 dark:text-white">
+	{@render children()}
+</h1>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,13 @@
 <script lang="ts">
 	import Modal from "$lib/components/modal/modal.svelte"
+	import H1 from "$lib/elements/h1.svelte"
 </script>
 
 <article>
-	<h1>Welcome to Thomas' Site Starter</h1>
+	<H1>Welcome to Thomas' Site Starter</H1>
+	<p>
+		This is my personal site starter template built with SvelteKit, Tailwind CSS, and TypeScript.
+	</p>
 </article>
 
 <Modal>
