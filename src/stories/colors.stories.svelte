fc40cab0d5a79fc93c3351965bcc9cd4eabe5b32
Thomas Eckert <thomas.james.eckert@gmail.com>
2025-06-02 23:26:33 -0400

    Add Tailwind colors

---
+++ src/stories/colors.stories.svelte
@@ -0,0 +1,22 @@
+<script module>
+	import { defineMeta } from "@storybook/addon-svelte-csf"
+	import Colors from "./colors.svelte"
+
+	const { Story } = defineMeta({
+		title: "Design System/Colors",
+		tags: ["autodocs"],
+		component: Colors,
+		argTypes: {
+			message: { control: "text" }
+		},
+		parameters: {
+			docs: {
+				description: {
+					component: ""
+				}
+			}
+		}
+	})
+</script>
+
+<Story name="Colors" />
---
+++ src/stories/dashboard.stories.svelte
@@ -0,0 +1,22 @@
+<script module>
+	import { defineMeta } from "@storybook/addon-svelte-csf"
+	import Dashboard from "./dashboard.svelte"
+
+	const { Story } = defineMeta({
+		title: "Layouts/Dashboard",
+		tags: ["autodocs"],
+		component: Dashboard,
+		argTypes: {
+			message: { control: "text" }
+		},
+		parameters: {
+			docs: {
+				description: {
+					component: ""
+				}
+			}
+		}
+	})
+</script>
+
+<Story name="Dashboard" />
