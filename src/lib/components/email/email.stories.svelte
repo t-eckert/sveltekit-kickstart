6b4a38596b4a4320d29109be31751e909d0e88a6
Thomas Eckert <thomas.eckert@redpanda.com>
2025-06-13 10:07:46 -0400

    Story for email

---
+++ src/lib/components/email/email.stories.svelte
@@ -0,0 +1,19 @@
+<script module>
+	import { defineMeta } from "@storybook/addon-svelte-csf"
+	import Email from "./email.svelte"
+
+	const { Story } = defineMeta({
+		title: "Components/Email",
+		tags: ["autodocs"],
+		component: Email,
+		parameters: {
+			docs: {
+				description: {
+					component: ""
+				}
+			}
+		}
+	})
+</script>
+
+<Story name="Email" args={{addr: "Dina.Rochino@gmail.com"}} />
