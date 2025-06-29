38eccf5e1eb55cfda7ba1d666c3dfa87319272ac
Thomas Eckert <thomas.james.eckert@gmail.com>
2025-06-28 21:50:09 -0400

    Tune the Accordion

--- src/lib/components/accordion/accordion.stories.svelte
+++ src/lib/components/accordion/accordion.stories.svelte
@@ -1,6 +1,6 @@
 <script module>
 	import { defineMeta } from "@storybook/addon-svelte-csf"
-	import Accordion from "./__story.svelte"
+	import Accordion from "./accordion.svelte"
 
 	const { Story } = defineMeta({
 		title: "Components/Accordion",
@@ -16,4 +16,27 @@
 	})
 </script>
 
-<Story name="Accordion" />
+<Story
+	name="Accordion"
+	args={{
+		items: [
+			{
+				value: "1",
+				title: "What is the meaning of life?",
+				content:
+					"To become a better person, to help others, and to leave the world a better place than you found it."
+			},
+			{
+				value: "2",
+				title: "How do I become a better person?",
+				content:
+					"Read books, listen to podcasts, and surround yourself with people who inspire you."
+			},
+			{
+				value: "3",
+				title: "What is the best way to help others?",
+				content: "Give them your time, attention, and love."
+			}
+		]
+	}}
+/>
--- src/lib/components/accordion/accordion.svelte
+++ src/lib/components/accordion/accordion.svelte
@@ -17,25 +17,28 @@
 
 <Accordion.Root class="w-full" type="multiple">
 	{#each items as item (item.value)}
-		<Accordion.Item value={item.value} class="group border-b border-neutral-300 px-2">
+		<Accordion.Item
+			value={item.value}
+			class="group border-b border-neutral-300 px-1 transition-colors hover:border-neutral-400 dark:border-neutral-700 dark:hover:border-neutral-600"
+		>
 			<Accordion.Header>
 				<Accordion.Trigger
-					class="flex w-full flex-1 items-center justify-start gap-3 py-3 text-[15px] font-medium transition-all select-none [&[data-state=open]>span>svg]:rotate-180"
+					class="flex w-full flex-1 cursor-pointer items-center justify-start gap-3 py-2 font-medium transition-all select-none [&[data-state=open]>span>svg]:rotate-180"
 				>
-					<span
-						class="hover:bg-dark-10 inline-flex size-6 items-center justify-center rounded-[7px] bg-transparent text-neutral-400"
-					>
-						<CaretDown class="size-[18px] transition-transform duration-200" />
-					</span>
 					<span class="w-full text-left leading-tight">
 						{item.title}
+					</span>
+					<span
+						class="hover:bg-dark-10 inline-flex size-6 items-center justify-center rounded-md bg-transparent text-neutral-400 group-hover:text-neutral-500 dark:text-neutral-500 dark:group-hover:text-neutral-400"
+					>
+						<CaretDown class="size-4 transition-transform duration-200" />
 					</span>
 				</Accordion.Trigger>
 			</Accordion.Header>
 			<Accordion.Content
 				class="data-[state=closed]:animate-accordion-up data-[state=open]:animate-accordion-down overflow-hidden text-sm tracking-[-0.01em]"
 			>
-				<div class="pb-[25px]">
+				<div class="pb-3">
 					{item.content}
 				</div>
 			</Accordion.Content>
