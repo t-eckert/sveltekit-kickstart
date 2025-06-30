0a4602fe73a46bf309f0ee84ceca5185f6daf77d
Thomas Eckert <thomas.james.eckert@gmail.com>
2025-06-29 22:08:05 -0400

    Pull over some useful components from Devy

---
+++ src/lib/components/analytics/analytics.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	import { browser } from "$app/environment"
+	import { page } from "$app/stores"
+	import { onMount } from "svelte"
+	import * as Fathom from "fathom-client"
+
+	interface Props {}
+
+	let {}: Props = $props()
+
+	onMount(async () => {
+		Fathom.load("SRGLORAK", {
+			url: "https://cdn.usefathom.com/script.js",
+			includedDomains: ["devy.page"]
+		})
+	})
+
+	$: $page.url.pathname, browser && Fathom.trackPageview()
+</script>
---
+++ src/lib/open-graph/og-header.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+	interface Props {
+		title: string
+		description?: string
+	}
+
+	const { title, description }: Props = $props()
+</script>
+
+<title>{title}</title>
+{#if description}
+	<meta name="description" content={description} />
+{/if}
+
+<meta property="og:type" content="website" />
+<meta property="og:title" content={title} />
+<meta property="og:image" content="https://devy.page/og-img.png" />
+
+<!-- Twitter Meta Tags -->
+<meta name="twitter:card" content="summary_large_image" />
+<meta property="twitter:domain" content="devy.page" />
+<meta name="twitter:title" content={title} />
+<meta name="twitter:description" content={description} />
+<meta name="twitter:image" content="https://devy.page/og-img.png" />
